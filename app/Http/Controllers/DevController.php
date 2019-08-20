c232261449b56c269a4767d4f49c5e03725c335b
Valentjno <valentinomerlino@gmail.com>
2019-08-20 23:47:06 +0200

    function store

--- app/Http/Controllers/DevController.php
+++ app/Http/Controllers/DevController.php
@@ -2,6 +2,8 @@
 
 
 namespace App\Http\Controllers;
+use App\Evidence;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class DevController extends Controller
@@ -14,5 +16,15 @@ class DevController extends Controller
     {
         $name=$request['name'];
         dd($name);
+        //NOME MODEL CON CUI STAI LAVORANDO (DOVE INSERISCI)
+        $id_device=NOMEMODEL::insertGetId([
+            'nome_device' => $request['t_i_id'],
+            'nome_gateway' => $id = Auth::user()->id,
+            'created_at'=> Carbon::now()->toDateTimeString(),
+            'updated_at'=> Carbon::now()->toDateTimeString(),
+        ]);
+        return response()->json([
+            'id' => $id_device,
+        ]);
     }
 }
