1d084dcbb3674f6ccad556491396b508c4809a6e
Valentjno <valentinomerlino@gmail.com>
2019-08-20 23:47:32 +0200

    function store of devcontroller

--- app/Http/Controllers/DevController.php
+++ app/Http/Controllers/DevController.php
@@ -17,14 +17,14 @@ class DevController extends Controller
         $name=$request['name'];
         dd($name);
         //NOME MODEL CON CUI STAI LAVORANDO (DOVE INSERISCI)
-        $id_device=NOMEMODEL::insertGetId([
-            'nome_device' => $request['t_i_id'],
-            'nome_gateway' => $id = Auth::user()->id,
-            'created_at'=> Carbon::now()->toDateTimeString(),
-            'updated_at'=> Carbon::now()->toDateTimeString(),
-        ]);
-        return response()->json([
-            'id' => $id_device,
-        ]);
+//        $id_device=NOMEMODEL::insertGetId([
+//            'nome_device' => $request['t_i_id'],
+//            'nome_gateway' => $id = Auth::user()->id,
+//            'created_at'=> Carbon::now()->toDateTimeString(),
+//            'updated_at'=> Carbon::now()->toDateTimeString(),
+//        ]);
+//        return response()->json([
+//            'id' => $id_device,
+//        ]);
     }
 }
